59b5146090e353db9ad35098af9a14ae9459772e
Brayen <brayenofz@gmail.com>
2024-08-30 03:46:50 -0300

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import pygame
+
+print("Setup start")
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480))
+print("Setup end")
+print("Loop start")
+while True:
+    # Check for all events
+    for events in pygame.event.get():
+        if events.type == pygame.QUIT:
+            pygame.quit()  # Close window
+            quit()  # end pygame
